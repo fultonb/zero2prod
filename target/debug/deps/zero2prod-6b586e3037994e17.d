e9351fdac883aa9729e30d6084dcf92cc94e6bab
Brad Fulton <fultonbd@gmail.com>
2021-09-10 12:50:34 -0700

    Test

---
+++ target/debug/deps/zero2prod-6b586e3037994e17.d
@@ -0,0 +1,5 @@
+/Users/bradfulton/Desktop/test1/zero2prod/target/debug/deps/zero2prod-6b586e3037994e17.rmeta: src/main.rs
+
+/Users/bradfulton/Desktop/test1/zero2prod/target/debug/deps/zero2prod-6b586e3037994e17.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/zero2prod-9ceb6171343e85c8.d
@@ -0,0 +1,5 @@
+/Users/bradfulton/Desktop/test1/zero2prod/target/debug/deps/zero2prod-9ceb6171343e85c8.rmeta: src/main.rs
+
+/Users/bradfulton/Desktop/test1/zero2prod/target/debug/deps/zero2prod-9ceb6171343e85c8.d: src/main.rs
+
+src/main.rs:
