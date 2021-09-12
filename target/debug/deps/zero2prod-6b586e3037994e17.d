82be49767232b0fa7cd02f13cd3cd48033c62f37
Brad Fulton <fultonbd@gmail.com>
2021-09-12 16:09:06 -0700

    Code (Chapter 3, Part 0)

--- target/debug/deps/zero2prod-6b586e3037994e17.d
+++
@@ -1,5 +0,0 @@
-/Users/bradfulton/Desktop/test1/zero2prod/target/debug/deps/zero2prod-6b586e3037994e17.rmeta: src/main.rs
-
-/Users/bradfulton/Desktop/test1/zero2prod/target/debug/deps/zero2prod-6b586e3037994e17.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/zero2prod-9ceb6171343e85c8.d
+++
@@ -1,5 +0,0 @@
-/Users/bradfulton/Desktop/test1/zero2prod/target/debug/deps/zero2prod-9ceb6171343e85c8.rmeta: src/main.rs
-
-/Users/bradfulton/Desktop/test1/zero2prod/target/debug/deps/zero2prod-9ceb6171343e85c8.d: src/main.rs
-
-src/main.rs:
